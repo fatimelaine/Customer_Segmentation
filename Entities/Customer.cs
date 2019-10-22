ed3fc134af6803e8265ef9ec35ac18247acc977b
“Elaine <fatima.elaine16@gmail.com>
2019-10-22 11:43:54 +0100

    Initial implementation

---
+++ Entities/Customer.cs
@@ -0,0 +1,60 @@
+﻿namespace CustomerSegmentation.Entities
+{
+    class Customer
+    {
+        public int ID { get; private set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double MonthlyAmountSpent { get; set; }
+        public bool GrowthPotential { get; set; }
+        public double MonthlyGoal { get; set; }
+
+        public Customer()
+        {
+        }
+
+        public Customer(int iD, string name, string email, double monthlyAmountSpent, bool growthPotential, double monthlyGoal)
+        {
+            ID = iD;
+            Name = name;
+            Email = email;
+            MonthlyAmountSpent = monthlyAmountSpent;
+            GrowthPotential = growthPotential;
+            MonthlyGoal = monthlyGoal;
+        }
+
+        public string Tier(double monthlyGoal, double monthlyAmountSpent, bool growthPotential)
+        {
+
+            if (monthlyAmountSpent < monthlyGoal && growthPotential == false)
+            {
+                return "Bronze Customer";
+            }
+            else if (monthlyAmountSpent < monthlyGoal && growthPotential == true)
+            {
+                return "Silver Customer";
+            }
+            else if (monthlyAmountSpent >= monthlyGoal && growthPotential == false)
+            {
+                return "Gold Customer";
+            }
+            else
+            {
+                return "Diamond Customer";
+            }
+        }
+
+        public override string ToString()
+        {
+            return "REPORT"
+                + "\nID: "
+                + ID
+                + ", Customer: "
+                + Name
+                + ", Email: "
+                + Email
+                + ", Tier: "
+                + Tier(MonthlyGoal, MonthlyAmountSpent, GrowthPotential);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+using CustomerSegmentation.Entities;
+using System.Collections.Generic;
+
+namespace CustomerSegmentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("How many customers will be registered? ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            List<Customer> list = new List<Customer>();
+
+            Console.Write("Monthly goal per customer: ");
+            double MonthlyGoal = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Customer #{i}: ");
+                Console.Write("ID: ");
+                int Id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string Name = Console.ReadLine();
+                Console.Write("Email: ");
+                string Email = Console.ReadLine();
+                Console.Write("Monthly Amount Spent: ");
+                double MonthlyAmountSpent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Growth Potential (True/False): ");
+                bool GrowthPotential = bool.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                list.Add(new Customer(Id, Name, Email, MonthlyAmountSpent, GrowthPotential, MonthlyGoal));
+            }
+
+            foreach (Customer cr in list)
+            {
+                Console.WriteLine(cr);
+            }
+        }
+    }
+}
